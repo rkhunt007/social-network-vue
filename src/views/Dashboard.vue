eada8074a0604ff3d5bdab3133083e3f4e814d5b
Rahul <rkhunt007@gmail.com>
2022-12-25 18:38:27 -0500

    Dashboard work

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,25 @@
 <template>
     <Header></Header>
     <div class="wrapper">
-        <h3>dashboard</h3>
-        <div v-for="user of users" :key="user._id">
-            <p>{{ user.name }}</p>
-        </div>
+        <h3 class="dashboard-title">Dashboard</h3>
+        <section id="friendsList">
+            <h3 class="friends-list-title">Friends List</h3>
+                <div>
+                    <article  v-for="user of users" :key="user._id" class="user">
+
+                        <h3>{{ user.user.name }}</h3>
+                        <p>{{ user.bio }}</p>
+                        <p>{{ user.city }}</p>
+                        <p>{{ user.profession }}</p>
+                        <img :src="user.user.image" :alt="user.user.name">
+                        <div>
+                            <button>Add</button>
+                        </div>
+
+                    </article>
+                    
+                </div>
+        </section>
     </div>
 </template>
 
@@ -30,29 +45,45 @@ export default {
             console.log(':::Dashboard:Comp:::getAllUsers');
             this.isLoading = true;
             try {
-				const res = await axios.get('users');
+                const res = await axios.get('users');
                 this.users = res.data;
-				this.isLoading = false;
-			} catch (error) {
-				let errorText = "";
-				if (error.response.data?.errors) {
-					error.response.data?.errors.forEach((err) => {
-						errorText += err.msg + " ";
-					});
-					if (!errorText) this.errorText = "Something went wrong! try again.";
-					this.errorText = errorText;
-				}
-				this.isLoading = false;
-			}
+                this.isLoading = false;
+            } catch (error) {
+                let errorText = "";
+                if (error.response.data?.errors) {
+                    error.response.data?.errors.forEach((err) => {
+                        errorText += err.msg + " ";
+                    });
+                    if (!errorText) this.errorText = "Something went wrong! try again.";
+                    this.errorText = errorText;
+                }
+                this.isLoading = false;
+            }
         }
     }
 }
 </script>
 
 <style scoped>
-
 .wrapper {
     margin: 10px;
 }
 
+.dashboard-title {
+    margin-left: 10px;
+}
+.friends-list-title {
+    margin-left: 10px;
+}
+
+.user {
+    width: calc(20% - 20px);
+    display: inline-block;
+    background: #d5ffdc;
+    margin: 10px;
+    padding: 10px;
+    box-sizing: border-box;
+    vertical-align: text-top;
+    box-shadow: 2px 2px 5px 1px rgba(0,0,0,0.5)
+}
 </style>
